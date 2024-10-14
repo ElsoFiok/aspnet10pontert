53a6cdeefad774733058efcb8797434df104ecc8
Elso Fiok <dstsit@hallgato.uni-neumanmn.hu>
2024-10-14 18:30:44 +0200

    Add Search

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -18,14 +18,36 @@ namespace MvcMovie.Controllers
         {
             _context = context;
         }
-
-        // GET: Movies
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string movieGenre, string searchString)
         {
-            return View(await _context.Movie.ToListAsync());
-        }
+            if (_context.Movie == null)
+            {
+                return Problem("Entity set 'MvcMovieContext.Movie'  is null.");
+            }
+            IQueryable<string> genreQuery = from m in _context.Movie
+                                            orderby m.Genre
+                                            select m.Genre;
+            var movies = from m in _context.Movie
+                        select m;
 
-        // GET: Movies/Details/5
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title!.ToUpper().Contains(searchString.ToUpper()));
+            }
+
+            if (!string.IsNullOrEmpty(movieGenre))
+            {
+                movies = movies.Where(x => x.Genre == movieGenre);
+            }
+
+            var movieGenreVM = new MovieGenreViewModel
+            {
+                Genres = new SelectList(await genreQuery.Distinct().ToListAsync()),
+                Movies = await movies.ToListAsync()
+            };
+
+            return View(movieGenreVM);
+        }
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -42,16 +64,10 @@ namespace MvcMovie.Controllers
 
             return View(movie);
         }
-
-        // GET: Movies/Create
         public IActionResult Create()
         {
             return View();
         }
-
-        // POST: Movies/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Create([Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
@@ -64,8 +80,6 @@ namespace MvcMovie.Controllers
             }
             return View(movie);
         }
-
-        // GET: Movies/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -80,10 +94,6 @@ namespace MvcMovie.Controllers
             }
             return View(movie);
         }
-
-        // POST: Movies/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Edit(int id, [Bind("Id,Title,ReleaseDate,Genre,Price")] Movie movie)
@@ -115,8 +125,6 @@ namespace MvcMovie.Controllers
             }
             return View(movie);
         }
-
-        // GET: Movies/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -133,8 +141,6 @@ namespace MvcMovie.Controllers
 
             return View(movie);
         }
-
-        // POST: Movies/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
