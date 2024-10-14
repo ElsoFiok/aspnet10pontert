8d2553b63a39345469f58d47787f49ae1e79f8b1
Elso Fiok <dstsit@hallgato.uni-neumanmn.hu>
2024-10-14 18:11:49 +0200

    Work with a database

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f55b159ee43dcd9790847e63c89ce3873a0ed13a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6639e5d3eda6d48e89962201ff32d52bd63ba651")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
